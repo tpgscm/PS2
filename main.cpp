069dae3fb6743c1f53ef0f1e262f2ba8b8564f07
tpgscm <kazooo853@gmail.com>
2014-07-08 13:22:25 +0900

    Update main.cpp
    MPとして機能追加

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 // master add function 2014/07/08 13:12
+int foo2( char *pDest, char *pSrc )       // mp add 2014/07/08 13:22
+{
+}
 int foo( void )
 {
 }
 int main( void )
 {
   foo();
+  foo2();                                 // mp add 2014/07/08 13:22
   return 0;
 }
